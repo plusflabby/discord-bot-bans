dfe7882fb1a5b16f5cb51ee4ea019cbaa9a7cfb6
+Flabby <plusflabby@gmail.com>
2024-01-01 02:21:15 -0500

    incude env

--- bot/test/all.js
+++ bot/test/all.js
@@ -2,6 +2,10 @@
 // ! Will run all test(s)
 // !
 
+// Require enviorment variables
+const dotenv = require('dotenv');
+dotenv.config();
+
 const API = require('./api.js');
 API.online().then(console.log);
 API.indentities().then(console.log);
