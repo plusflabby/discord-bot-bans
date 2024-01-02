49af9257142bfd4491f7b1e64f7c9f2a0ad4bf69
+Flabby <plusflabby@gmail.com>
2024-01-01 20:58:25 -0500

    sati

--- bot/test/all.js
+++ bot/test/all.js
@@ -1,10 +1,6 @@
 // !
 // ! Will run all test(s)
 // !
-console.log(process.argv[process.argv.indexOf('apikey')-1])
-console.log(process.argv[process.argv.indexOf('apikey')+1])
-console.log(process.argv[process.argv.indexOf('apikey')])
-console.log(String(process.argv[2]).split('=')[1])
 const API = require('./api.js');
 API.online().then(console.log);
 API.indentities().then(console.log);
