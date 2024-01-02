03fecf6d86d6b018dd42d21338fcb331f6d4bd71
+Flabby <plusflabby@gmail.com>
2024-01-01 20:44:27 -0500

    Fix(s)

--- bot/test/all.js
+++ bot/test/all.js
@@ -1,7 +1,7 @@
 // !
 // ! Will run all test(s)
 // !
-console.log(process.env)
+console.log(process.argv)
 const API = require('./api.js');
 API.online().then(console.log);
 API.indentities().then(console.log);
